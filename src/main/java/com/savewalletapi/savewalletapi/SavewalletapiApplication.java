6ab684e9d35fae5911cb87c73fe8405a03b28833
afriddev <afridayan01@gmail.com>
2025-02-11 01:52:38 +0530

    added base code

---
+++ src/main/java/com/savewalletapi/savewalletapi/SavewalletapiApplication.java
@@ -0,0 +1,13 @@
+package com.savewalletapi.savewalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavewalletapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavewalletapiApplication.class, args);
+	}
+
+}
